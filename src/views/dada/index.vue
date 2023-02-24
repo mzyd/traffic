2efecaf3bafb7ab8c7442c661a8498fc19886c18
Seabiscuit <mzy3991@gmail.com>
2023-02-24 16:29:23 +0800

    curve

--- src/views/dada/index.vue
+++ src/views/dada/index.vue
@@ -12,7 +12,7 @@
 import { ref, reactive, onMounted } from 'vue'
 import * as THREE from 'three'
 import { OrbitControls } from '@three-ts/orbit-controls'
-import { useLoaderByGLB } from './loadModel.ts'
+import { useLoaderByGLB, useLoaderByGLTF } from './loadModel.ts'
 
 import gsap from 'gsap'
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin.js'
@@ -24,47 +24,63 @@ let animateGasp: gsap.core.Tween | null
 const dada = ref<HTMLElement | null>(null)
 
 let old = null
+let scene = null
+let camera = null
+let renderer = null
 
 const listNorth = ref([
-  { x: 6, y: 0, z: -30 },
-  { x: 6, y: 0, z: -60 },
-  { x: 6, y: 0, z: -90 },
-  { x: -6, y: 0, z: -40 }
+  { x: 6, y: 0, z: -30, type: 0 },
+  { x: 6, y: 0, z: -60, type: 1 },
+  { x: 6, y: 0, z: -90, type: 0 },
+  { x: -6, y: 0, z: -40, type: 1 }
 ])
 
 onMounted(() => {
   initThree(dada)
+  initCurve()
 })
 
+const initCurve = () => {
+  const initialPoints = [
+    { x: -10, y: 0, z: 0 },
+    { x: 0, y: 0, z: 0 },
+    { x: 0, y: 0, z: 10 }
+  ]
+
+  //Create a closed wavey loop
+  const curve = new THREE.CatmullRomCurve3(initialPoints.map(i => new THREE.Vector3(i.x, i.y, i.z)))
+  const points = curve.getPoints(50)
+  const geometry = new THREE.BufferGeometry().setFromPoints(points)
+  const material = new THREE.LineBasicMaterial({ color: 0xf21010 })
+  // Create the final object to add to the scene
+  const curveObject = new THREE.Line(geometry, material)
+  console.log("ccccccc", curveObject)
+  scene.add(curveObject)
+
+  const addCube = (pos) => {
+    console.log("....pos...", pos)
+    const geometry = new THREE.BoxGeometry(3, 3, 3)
+    const material = new THREE.MeshBasicMaterial(0x00ff04)
+    const cube = new THREE.Mesh(geometry, material)
+    cube.position.copy(pos)
+    scene.add(cube)
+    return cube
+  }
+  const cubeList = initialPoints.map(pos => addCube(pos))
+  console.log("cuge list ", cubeList)
+
+}
+
 const initThree = async (instance: HTMLElement | null) => {
-  const scene = new THREE.Scene()
-  scene.background = new THREE.Color(0xffffff)
-  const camera = new THREE.PerspectiveCamera(
-    75, // 视野角度
-    window.innerWidth / window.innerHeight, // 屏幕比例
-    1, // 近截面
-    1000 // 远截面
-  )
-  camera.position.y = 120
-  camera.position.z = 150 // 远近值
+  setupScene()
+  setupCamera()
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 4) // 创建环境光
-  scene.add(ambientLight) // 将环境光添加到场景
-
-  // 200表示网格模型的尺寸大小，25表示纵横细分线条数量
-  const gridHelper = new THREE.GridHelper(800, 30)
-  scene.add(gridHelper)
-
-  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.outputEncoding = THREE.sRGBEncoding
-
   instance.value.appendChild(renderer.domElement)
 
-  const geometry = new THREE.BoxGeometry(5, 5, 5)
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-  const cube = new THREE.Mesh(geometry, material)
-  scene.add(cube)
+  setupBox(renderer)
 
   const controls = new OrbitControls(camera, renderer.domElement)
   controls.minPolarAngle = 0
@@ -75,40 +91,75 @@ const initThree = async (instance: HTMLElement | null) => {
   // this.enableZoom = true // Set to false to disable zooming
   // this.zoomSpeed = 1.0
 
+
+  // const road = await useLoaderByGLB(scene, '/lukou/scene.glb', (glb) => {
+  //   glb.scene.position.set(0, 0, 0)
+  //   glb.scene.scale.set(2.0, 2.0, 2.0)
+  // })
+
+  // const tokyo = await useLoaderByGLB(scene, '/tokyo/scene.glb', (glb) => {
+  //   glb.scene.scale.set(10, 10, 10)
+  //   glb.scene.position.set(26, 0, 20)
+  // })
+
+  // listNorth.value.forEach(async (item) => {
+  //   const { x, y, z } = item
+  //   const path = item.type === 0 ? '/src/assets/model/rusty-car/' : '/src/assets/model/truck/'
+  //   console.log("path-->", path)
+  //   await useLoaderByGLB(scene, path, (glb) => {
+  //     glb.scene.scale.set(0.1, 0.1, 0.1)
+  //     glb.scene.position.set(x, y, z)
+  //   })
+  // })
+
+  const oldCar = await useLoaderByGLB(scene, '/rusty-car/scene.glb', (glb) => {
+    glb.scene.scale.set(0.1, 0.1, 0.1)
+    glb.scene.position.set(6, 0, -30)
+  })
+
+  const oldCar2 = await useLoaderByGLB(scene, '/rusty-car/scene.glb', (glb) => {
+    glb.scene.scale.set(0.1, 0.1, 0.1)
+    glb.scene.position.set(-90, 0, -4)
+    old = glb.scene
+    glb.scene.rotation.y = Math.PI / 2
+  })
+  testGo(oldCar)
+}
+
+function setupScene() {
+  scene = new THREE.Scene()
+  scene.background = new THREE.Color(0xffffff)
+  const ambientLight = new THREE.AmbientLight(0x333333, 4) // 创建环境光
+  scene.add(ambientLight) // 将环境光添加到场景
+  // 200表示网格模型的尺寸大小，25表示纵横细分线条数量
+  const gridHelper = new THREE.GridHelper(800, 30)
+  scene.add(gridHelper)
+}
+
+function setupBox(renderer) {
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
   const animate = () => {
     requestAnimationFrame(animate)
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
     renderer.render(scene, camera)
   }
+
   animate()
-
-  const road = await useLoaderByGLB(scene, '/src/assets/model/road/', (glb) => {
-    glb.scene.scale.set(0.1, 0.1, 0.1)
-    glb.scene.position.set(16, 0, 20)
-  })
-
-  listNorth.value.forEach(async (item) => {
-    const { x, y, z } = item
-    await useLoaderByGLB(scene, '/src/assets/model/rusty-car/', (glb) => {
-      glb.scene.scale.set(0.1, 0.1, 0.1)
-      glb.scene.position.set(x, y, z)
-    })
-  })
-
-  const oldCar = await useLoaderByGLB(scene, '/src/assets/model/rusty-car/', (glb) => {
-    glb.scene.scale.set(0.1, 0.1, 0.1)
-    glb.scene.position.set(6, 0, -30)
-  })
-
-  const oldCar2 = await useLoaderByGLB(scene, '/src/assets/model/rusty-car/', (glb) => {
-    glb.scene.scale.set(0.1, 0.1, 0.1)
-    glb.scene.position.set(-90, 0, -4)
-    old = glb.scene
-    glb.scene.rotation.y = Math.PI / 2
-  })
-
-  testGo(oldCar)
+}
+function setupCamera() {
+  camera = new THREE.PerspectiveCamera(
+    75, // 视野角度
+    window.innerWidth / window.innerHeight, // 屏幕比例
+    1, // 近截面
+    1000 // 远截面
+  )
+  camera.position.y = 100
+  camera.position.z = 100 // 远近值
 }
 
 function handleTurn() {
@@ -140,7 +191,6 @@ function testGo(obj) {
 function handleReset() {
   animateGasp.reverse()
 }
-
 function handlePlay() {
   animateGasp.play()
 }
--- src/views/dada/loadModel.ts
+++ src/views/dada/loadModel.ts
@@ -1,22 +1,65 @@
 // import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+// import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import _ from 'lodash-es'
 
 const gltfLoader = new GLTFLoader()
+const basePath = '/src/assets/models'
 
 export function useLoaderByGLB(scene: any, path: string, callback: Function) {
+	// const dracoLoader = new DRACOLoader()
+	// dracoLoader.setDecoderPath( '/src/assets/model/tokyo/' )
+  // const p = basePath + _.dropRight(path.split('/').toString().replace(',', '/'))
+  // .join('')
+  const modelName = _.last(path.split('/'))
+  const _p = basePath + _.dropRight(path.split('/')).join(',').replaceAll(',', '/')
+  const _path = _.last(_p.split('')) !== '/' ? _p + '/' : _p
+  return new Promise((resolve, reject) => {
+    // gltfLoader.setDRACOLoader(dracoLoader)
+    gltfLoader.setPath(_path)
+    gltfLoader.load(
+      modelName,
+      (glb: any) => {
+        glb.scene.traverse((child: any) => {
+          if (child.isMesh) {
+            // child.material.metalness = 0
+            // child.material.emissiveMap = child.material.map
+          }
+        })
+        callback(glb)
+        scene.add(glb.scene)
+        resolve(glb)
+      },
+      undefined,
+      (err: any) => {
+        console.log('Load Error ----> ', err)
+        reject(err)
+      }
+    )
+  })
+}
+
+export function useLoaderByGLTF(scene: any, path: string, callback: Function) {
   return new Promise((resolve, reject) => {
     gltfLoader.setPath(path)
-    gltfLoader.load('scene.glb', (glb: any) => {
-      glb.scene.traverse((child: any) => {
-        if (child.isMesh) {
-          // child.material.emissiveMap = child.material.map
-        }
-      })
-
-      callback(glb)
-
-      scene.add(glb.scene)
-      resolve(glb)
-    })
+    gltfLoader.load(
+      'scene.gltf',
+      (glb: any) => {
+        glb.scene.traverse((child: any) => {
+          if (child.isMesh) {
+            // console.log("mmmmmmm", child)
+            child.material.emissiveMap = child.material.map
+          }
+        })
+        callback(glb)
+        scene.add(glb.scene)
+        resolve(glb)
+      },
+      undefined,
+      (err: any) => {
+        console.log('Load Error ----> ', err)
+        reject(err)
+      }
+    )
   })
 }
